72523a13df869af8bc40564629bcc1902f7ba5ae
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 05:11:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 board = [""]
 index = 0
 
-def valid_move(move)
-  if position_taken?(array, index)
+def valid_move(board, index)
+  if position_taken?(array, ind)
     puts "true"
     return true
   else
@@ -14,7 +14,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 
-def position_taken?(array, idx)
+def position_taken?(array, ind)
  array.each.with_index(1) do |value, idx|
    if value == "" || value == " " || value == nil
      puts "false"
