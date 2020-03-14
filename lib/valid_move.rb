66fb7a0fea47eff29b27a2808d353b6a44286e1f
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 04:46:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
+board = [""]
+index = 0
 
+def valid_move(Input) 
+  if position_taken? 
+    return false 
+  else 
+    return true  
+  end 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+def position_taken?(array, index)
+ array.each.with_index(1) do |value, idx|
+   if value == "" || value == " " || value == nil
+     puts "false"
+     return false
+   elsif value == "X" || value == "O"
+     puts "true"
+     return true
+   end
+  end
+ end
